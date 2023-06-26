0c4219461a74b767fe2bb952ce58e51dcb717b88
KaranRaval <117911284+KaranRaval123@users.noreply.github.com>
2023-06-26 16:47:20 +0530

    Delete colorflipper directory

--- colorflipper/script.js
+++
@@ -1,8 +0,0 @@
-flipcolor = () =>{
-    document.getElementById('h').innerText="";
-    let random = Math.floor(Math.random() * 16777215);
-    console.log(random);
-    let color = '#'+random.toString(16);
-    document.body.style.backgroundColor = color;
-    document.getElementById('h').innerText=color;
-}
