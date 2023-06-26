f402410edb81e11965b32690ac00e4b5352bbdea
KaranRaval <117911284+KaranRaval123@users.noreply.github.com>
2023-06-26 15:08:58 +0530

    Add files via upload

---
+++ Todolist/script.js
@@ -0,0 +1,11 @@
+let input = document.getElementById("inputtext");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+  if(input.value.length>0){
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value))
+    ul.appendChild(li);
+    }
+})
